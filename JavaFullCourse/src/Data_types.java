2a96f5fb02bd7793faeafb2f52d80353835c01d4
fouzan u <fouzan u@DESKTOP-IOE0TJI>
2022-04-17 11:41:54 +0530

    First Commit

---
+++ JavaFullCourse/src/Data_types.java
@@ -0,0 +1,14 @@
+
+public class Data_types {
+		
+	public static void main(String[] args) {
+		int a = 10;
+		float f = 22.2f;
+		double d = 222.2222;
+		boolean b = true;
+		String name = "Syed";
+		char letter = 'd';
+		System.out.println(a+" "+f+" "+d+" "+b+" "+name+" "+letter);
+	}
+
+}
