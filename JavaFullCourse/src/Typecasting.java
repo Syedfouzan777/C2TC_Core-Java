24dd3a54d1bb392a4d5f9ccc05f80853d0a5174a
fouzan u <fouzan u@DESKTOP-IOE0TJI>
2022-04-17 11:52:41 +0530

    First Commit

---
+++ JavaFullCourse/src/Typecasting.java
@@ -0,0 +1,17 @@
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		int a =10;
+		double d = a; //Widening casting
+		System.out.println(d);
+		System.out.println(a);
+		
+		double my_double = 9.78;
+		int myInt = (int) my_double; //Narrow castiing
+		System.out.println(my_double);
+		System.out.println(myInt);
+
+	}
+
+}
