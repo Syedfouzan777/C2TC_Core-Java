439f638bd2f5fd46a4b58c1756d4f8b96d6ef562
fouzan u <fouzan u@DESKTOP-IOE0TJI>
2022-04-17 14:34:26 +0530

    First Commit

---
+++ JavaFullCourse/src/Nested_if.java
@@ -0,0 +1,19 @@
+
+public class Nested_if {
+
+	public static void main(String[] args) {
+		String lang = "Python";
+		if(lang == "c") {
+			if(lang == "c++")
+				System.out.println("Lang is c++");
+			}else if(lang == "java") {
+				System.out.println("Lang is java");
+			}else if(lang == "Python") {
+				System.out.println("Lang is python");
+			}else {
+				System.out.println("Null");
+			}
+		
+	}
+
+}
