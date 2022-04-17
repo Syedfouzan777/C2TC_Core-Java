28f29a78eea7f972d89014f05bed917df534ff53
fouzan u <fouzan u@DESKTOP-IOE0TJI>
2022-04-17 14:24:59 +0530

    First Commit

---
+++ JavaFullCourse/src/conditon_statements.java
@@ -0,0 +1,14 @@
+
+public class conditon_statements {
+
+	public static void main(String[] args) {
+		int age = 15;
+		if(age>=18 & age<=60) {
+			System.out.println("you can drive");
+		}else {
+			System.out.println("You cannot drive");
+		}
+	}
+
+}
+
---
+++ JavaFullCourse/src/else_if.java
@@ -0,0 +1,18 @@
+
+public class else_if {
+
+	public static void main(String[] args) {
+				int b =10;
+				if(b>=18) {
+					System.out.println("You can drive");
+				}else if(b<18) {
+					System.out.println("You are not eliglible");
+				}else if(b<65) {
+					System.out.println("You are retired");
+				}else {
+					System.out.println("You can drive");
+				}
+				
+	}
+
+}
