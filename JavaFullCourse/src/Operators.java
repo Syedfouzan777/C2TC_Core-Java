1aa983262c60a0b0443b8a1602b8e60dcf6e497f
fouzan u <fouzan u@DESKTOP-IOE0TJI>
2022-04-17 14:03:35 +0530

    First Commit

---
+++ JavaFullCourse/src/Operators.java
@@ -0,0 +1,21 @@
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = ++a;
+		int d = a++;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.println(a++);
+		System.out.println(b--);
+		System.out.println(++a);
+		System.out.println(--a);
+
+	}
+
+}
