de7cb7741b57596d278784ab183b91e075b96a79
fouzan u <fouzan u@DESKTOP-IOE0TJI>
2022-04-19 06:33:00 +0530

    First Commit

---
+++ JavaFullCourse/src/Condition_if.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Condition_if {
+
+	public static void main(String[] args) {
+		int pwd;
+		System.out.println("Enter password: ");
+		Scanner obj=new Scanner(System.in);
+		pwd=obj.nextInt();
+		if(pwd==7041)
+		{
+			System.out.println("You entered correct password here is the information");
+			System.out.println("My name is Syed Fouzan");
+			System.out.println("My age is 23");
+
+
+		}
+	}
+
+}
