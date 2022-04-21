4e34233c7637ecd89f6080221ffc30dac1e64cd4
fouzan u <fouzan u@DESKTOP-IOE0TJI>
2022-04-21 06:20:32 +0530

    First commit

--- JavaFullCourse/src/else_if.java
+++ JavaFullCourse/src/else_if.java
@@ -1,16 +1,18 @@
+import java.util.Scanner;
 
 public class else_if {
 
 	public static void main(String[] args) {
-				int b =10;
-				if(b>=18) {
+				int b;
+				System.out.println("Enter Your age: ");
+				Scanner s=new Scanner(System.in);
+				b=s.nextInt();
+				if(b>=18 && b<65) {
 					System.out.println("You can drive");
-				}else if(b<18) {
+				}else if(b<18 ) {
 					System.out.println("You are not eliglible");
-				}else if(b<65) {
-					System.out.println("You are retired");
 				}else {
-					System.out.println("You can drive");
+					System.out.println("You can't drive");
 				}
 				
 	}
--- JavaFullCourse/src/while_loop.java
+++ JavaFullCourse/src/while_loop.java
@@ -1,18 +1,22 @@
+import java.util.Scanner;
 
 public class while_loop {
 
 	public static void main(String[] args) {
-			int i = 1;
-			while(i<=10){
+			int num;
+			System.out.println("Enter any number: ");
+			Scanner a=new Scanner(System.in);
+			num = a.nextInt();
+			while(num>=0) {
+				if(num%2==0) {
+					System.out.println("Even number");
+				}
+				else {
+					System.out.println("odd number");
 
-			System.out.println(i);
-			i++;
-
+				}
 			}
 	}
 
 }
-
-	
-
 
